58d5379a110765e00c51cd8a801525a286098efe
deeelia <Deelia.pv@gmail.com>
2019-11-27 12:44:02 -0500

    mi primera colaboracion

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter wil title',
+      title: 'Flutter wil title colaborador delia',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter git wil'),
+      home: MyHomePage(title: 'Flutter git will colaborador delia'),
     );
   }
 }
